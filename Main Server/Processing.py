259edc2f13d72cc542207a5858b73599e96f1c67
esh4 <eshel.sinclair@gmail.com>
2016-12-08 16:42:56 +0200

    create basic framework for threading

--- Main Server/Processing.py
+++ Main Server/Processing.py
@@ -2,6 +2,7 @@ from GlobalData import HSV_highThresh
 from GlobalData import HSV_lowThresh
 from GlobalData import currentFrame
 from GlobalData import hsvImg
+from GlobalData import posData
 import cv2
 import threading
 
@@ -9,6 +10,14 @@ import threading
 class ImageProcess(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
+
+    def run(self):
+        global currentFrame
+        global hsvImg
+        global posData
+
+        currentFrame = self.capImg(0)
+        hsvImg, posData = self.filterImg(currentFrame)
 
     def capImg(self, camIndex):
         cam = cv2.VideoCapture(camIndex)
@@ -21,4 +30,5 @@ class ImageProcess(threading.Thread):
         global HSV_highThresh
         HSVimg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         filtHSVimg = cv2.inRange(HSVimg, HSV_lowThresh, HSV_highThresh)
-        return filtHSVimg
+        data = []
+        return filtHSVimg, data
